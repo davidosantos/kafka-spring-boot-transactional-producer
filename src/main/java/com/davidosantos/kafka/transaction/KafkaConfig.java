b18cc458ba14289a40a896119f7ce60aa8a11e46
davidosantos <davidosantos@hotmail.com>
2022-11-28 15:15:23 -0300

    .

--- src/main/java/com/davidosantos/kafka/transaction/KafkaConfig.java
+++ src/main/java/com/davidosantos/kafka/transaction/KafkaConfig.java
@@ -4,16 +4,10 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class KafkaConfig {
-
-    @Bean
-    public NewTopic transactionsTopic() {
-       return TopicBuilder.name("transactions")
-           .partitions(3)
-           .replicas(3)
-           .build();
-    }
     
 }
